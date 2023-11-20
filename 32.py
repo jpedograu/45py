a8829721bb48eed2dc83dfeb48071282129f53d4
jpedograu <144135652+jpedograu@users.noreply.github.com>
2023-11-20 07:54:44 -0300

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,7 +1,7 @@
-nt=float(input('sua primeira nota carinha:'))
-nt2=float(input('sua primeira nota carinha:'))
+nt=float(input('sua primeira nota carinha legal:'))
+nt2=float(input('sua primeira nota carinha legal:'))
 md=(nt+nt2)/2
 if md>=6:
-    print('vc foi aprovado carinha')
+    print('vc foi aprovado carinha legal')
 else:
-    print('vc foi reprovado')
+    print('vc foi reprovado :(')
